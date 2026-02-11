857f0b132bb5c4b7719ac2c816389a58bcd4d4c1
glebBeliaev <flysolo.project@gmail.com>
2026-02-11 18:05:15 +0100

    module-8-7

---
+++ learning/internal/link/model.go
@@ -0,0 +1,43 @@
+package link
+
+import (
+	"math/rand"
+
+	"gorm.io/gorm"
+)
+
+type Link struct {
+	gorm.Model
+	Url  string `json:"url"`
+	Hash string `json:"hash" gorm:"uniqueIndex"`
+}
+
+func NewLink(url string) *Link {
+	return &Link{
+		Url:  url,
+		Hash: RandStringRunes(10),
+	}
+}
+
+var letterRunes = getEnglishAlphabet()
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func getEnglishAlphabet() []rune {
+	var alphabet []rune
+	// Добавляем заглавные буквы A-Z (коды Unicode: 65–90)
+	for i := 65; i <= 90; i++ {
+		alphabet = append(alphabet, rune(i))
+	}
+	// Добавляем строчные буквы a-z (коды Unicode: 97–122)
+	for i := 97; i <= 122; i++ {
+		alphabet = append(alphabet, rune(i))
+	}
+	return alphabet
+}
---
+++ learning/migrations/auto.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"http/learning/internal/link"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load("learning/.env")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&link.Link{})
+
+}
